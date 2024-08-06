6071e974616db802cff8d7e558b7c8f718dc3f60
Rahul Aher <rahul.aher@morningstar.com>
2024-08-06 16:58:32 +0530

    making callout card responsive

--- src/components/CalloutCard.vue
+++ src/components/CalloutCard.vue
@@ -13,22 +13,15 @@
 					<p class="content__title">
 						{{ title }}
 					</p>
-
+  
 					<span class="content__text">
-						<!-- @slot Slot used to insert customized content into the text. -->
 						<slot name="text">
 							{{ text }}
 						</slot>
 					</span>
 				</div>
 				<div>
-					<!-- @slot Slot used to insert customized content in place of the action button. -->
 					<slot name="action">
-						<!--
-							Event that indicates that the Button was clicked
-							@event action-button-click
-							@type {Event}
-						-->
 						<rds-button
 							id="content-button"
 							:variant="actionButtonVariant"
@@ -43,83 +36,56 @@
 		</div>
 	</span>
 </template>
-
+  
 <script>
 import RdsButton from './Button.vue';
-
+  
 export default {
 	components: {
 		RdsButton,
 	},
-
+  
 	props: {
-		/**
-		 * The title of the card. The title can also be used with the slot.
-		 */
 		title: {
 			type: String,
 			default: 'AlertCard title',
 			required: true,
 		},
-		/**
-		 * The text of the card. Text can also be used with slot.
-		 */
 		text: {
 			type: String,
 			default: 'AlertCard Subtitle',
 		},
-		/**
-		 * The card image.
-		 */
 		image: {
 			type: String,
 			default: '',
 			required: true,
 		},
-		/**
-		 * The description of the card image.
-		 */
 		imageDescription: {
 			type: String,
 			default: 'Image by CalloutCard',
 		},
-		/**
-		 * The color of the card button.
-		 */
 		actionButtonVariant: {
 			type: String,
 			default: 'green',
 		},
-		/**
-		 * The card button text.
-		 */
 		actionButtonText: {
 			type: String,
 			default: 'Ok',
 		},
-		/**
-		 * The minor version of the button.
-		 */
 		actionButtonSecondary: {
 			type: Boolean,
 			default: false,
 		},
-		/**
-		 * The disabled version of the button.
-		 */
 		actionButtonDisabled: {
 			type: Boolean,
 			default: false,
 		},
-		/**
-		 * Card display mode.
-		 */
 		compact: {
 			type: Boolean,
 			default: false,
 		},
 	},
-
+  
 	methods: {
 		resolveClass(element) {
 			return this.compact ? `${element}--compact` : element;
@@ -127,70 +93,70 @@ export default {
 	},
 };
 </script>
-
-<style lang="scss">
-@import '../assets/sass/tokens.scss';
-
-#callout-card .callout-card {
+  
+  <style lang="scss">
+  @import '../assets/sass/tokens.scss';
+  
+  #callout-card .callout-card {
 	align-items: center;
 	display: flex;
 	border: 1px solid $n-40;
 	border-radius: 16px;
 	padding: pa(7);
-
-	&__container {
-		@extend .callout-card;
-		width: 700px;
-
+  
+		&__container {
+			@extend .callout-card;
+			width: 700px;
+  
 		&--compact {
 			@extend .callout-card;
 			padding: pYX(2,6);
 		}
 	}
-
+  
 	&__content-container {
-		width: 20%;
+		width: 100%;
 	}
-}
-
-#callout-card .image {
+  }
+  
+  #callout-card .image {
 	&__container {
 		margin: mr(7);
 	}
-
+  
 	&__content {
 		max-height: 150px;
 		max-width: 150px;
-
+	
 		&--compact {
 			max-height: 90px;
 			max-width: 120px;
 		}
 	}
-}
-
-#callout-card .content {
+  }
+  
+  #callout-card .content {
 	margin: mb(4);
-
+  
 	&--compact {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		max-width: 529px;
 	}
-
+  
 	&__title {
 		color: $n-700;
 		display: block;
 		@include subheading-3;
 		margin: mb(2);
 	}
-
+  
 	&__text {
 		color: $n-700;
 		@include body-2;
 	}
-
+  
 	&__container {
 		&--compact {
 			align-items: center;
@@ -199,5 +165,44 @@ export default {
 			width: 100%;
 		}
 	}
-}
-</style>
+  }
+  
+  @media screen and (max-width: 768px) {
+	#callout-card .callout-card {
+		flex-direction: column;
+		align-items: flex-start;
+	
+		&__container {
+			width: 100%;
+		}
+	
+		&__content-container {
+			width: 100%;
+			margin-top: 1rem;
+		}
+	
+		.image__content {
+			max-height: 100px;
+			max-width: 100px;
+		}
+	}
+  }
+  
+  @media screen and (max-width: 480px) {
+	#callout-card .callout-card {
+		&__container {
+			padding: pa(4);
+		}
+	
+		.content__title {
+			@include subheading-3;
+		}
+	
+		.image__content {
+			max-height: 75px;
+			max-width: 75px;
+		}
+	}
+  }
+  </style>
+  
